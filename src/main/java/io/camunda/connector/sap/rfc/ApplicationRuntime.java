bae974d99f03982a654ced755f6d120f455026f9
Volker Buzek <6573426+vobu@users.noreply.github.com>
2024-10-08 17:32:21 +0200

    ci/e2e-on-btp (#3)
    make the connector work on BTP, 
    with RFC destination to on-prem SAP Sys,
    connected to C8 SaaS

--- src/main/java/io/camunda/connector/sap/rfc/ApplicationRuntime.java
+++ src/main/java/io/camunda/connector/sap/rfc/ApplicationRuntime.java
@@ -2,9 +2,10 @@ package io.camunda.connector.sap.rfc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ApplicationRuntime {
+public class ApplicationRuntime extends SpringBootServletInitializer {
 
   public static void main(String[] args) {
     SpringApplication.run(ApplicationRuntime.class, args);
