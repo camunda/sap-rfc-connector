8290b838953c9518e6167977f444872d51a01fe9
Volker Buzek <volker.buzek@camunda.com>
2024-12-11 18:01:37 +0100

    refactor: reformat via mvn spotless

--- src/test/java/io/camunda/connector/sap/rfc/e2eTest.java
+++ src/test/java/io/camunda/connector/sap/rfc/e2eTest.java
@@ -2,7 +2,6 @@ package io.camunda.connector.sap.rfc;
 
 import io.camunda.zeebe.client.ZeebeClient;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.EnabledIf;
 import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 
 @EnabledIfEnvironmentVariable(named = "e2e", matches = "true")
@@ -13,20 +12,18 @@ public class e2eTest {
   e2eTest() {
     // the evn vars are set in the github action
     // derived from the repo secrets
-    zeebeClient = ZeebeClient.newCloudClientBuilder()
-        .withClusterId(System.getenv("clusterId"))
-        .withClientId(System.getenv("clientId"))
-        .withClientSecret(System.getenv("clientSecret"))
-        .withRegion(System.getenv("region"))
-        .build();
+    zeebeClient =
+        ZeebeClient.newCloudClientBuilder()
+            .withClusterId(System.getenv("clusterId"))
+            .withClientId(System.getenv("clientId"))
+            .withClientSecret(System.getenv("clientSecret"))
+            .withRegion(System.getenv("region"))
+            .build();
   }
 
   @Test
   void bapi() {
-    zeebeClient.newDeployResourceCommand()
-        .addResourceFromClasspath("bapi.bpmn")
-        .send()
-        .join();
+    zeebeClient.newDeployResourceCommand().addResourceFromClasspath("bapi.bpmn").send().join();
     var processInstanceResult =
         zeebeClient
             .newCreateInstanceCommand()
@@ -35,7 +32,5 @@ public class e2eTest {
             .withResult()
             .send()
             .join();
-
-
   }
 }
