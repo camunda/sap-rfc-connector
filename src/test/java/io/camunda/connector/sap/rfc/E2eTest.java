0505fb63bf491d731e8804d0c1fe87126943c97b
Volker Buzek <volker.buzek@camunda.com>
2024-12-12 11:48:36 +0100

    test: bapi + rfm

--- src/test/java/io/camunda/connector/sap/rfc/E2eTest.java
+++ src/test/java/io/camunda/connector/sap/rfc/E2eTest.java
@@ -1,10 +1,15 @@
 package io.camunda.connector.sap.rfc;
 
+import io.camunda.connector.sap.rfc.model.BapiConnectorResponse;
 import io.camunda.zeebe.client.ZeebeClient;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @EnabledIfEnvironmentVariable(named = "e2e", matches = "true")
 public class E2eTest {
@@ -23,6 +28,7 @@ public class E2eTest {
             .withClientSecret(System.getenv("clientSecret"))
             .withRegion(System.getenv("region"))
             .build();
+
   }
 
   @Test
@@ -38,7 +44,26 @@ public class E2eTest {
             .send()
             .join();
 
-    LOGGER.info("//> test run output vars");
-    processInstanceResult.getVariablesAsMap().forEach((k, v) -> LOGGER.info("{}: {}", k, v));
+    var result = processInstanceResult.getVariablesAsMap();
+    BapiConnectorResponse costCenter = (BapiConnectorResponse) result.get("CostCenter");
+    int costCenterListSize = ((ArrayList) costCenter.tables().get("COSTCENTERLIST")).size();
+    assertTrue(costCenterListSize >= 1, costCenterListSize + " cost center returned");
+  }
+
+  @Test
+  void rfm() {
+    zeebeClient.newDeployResourceCommand().addResourceFromClasspath("rfm.bpmn").send().join();
+
+    var processInstanceResult =
+        zeebeClient
+            .newCreateInstanceCommand()
+            .bpmnProcessId("rfm")
+            .latestVersion()
+            .withResult()
+            .send()
+            .join();
+
+    LOGGER.info("//> rfm result");
+    LOGGER.info(processInstanceResult.getVariablesAsMap().toString());
   }
 }
