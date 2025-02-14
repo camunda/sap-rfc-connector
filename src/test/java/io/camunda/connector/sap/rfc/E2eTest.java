19ff65fc2e6e5d18c181c58a51df1298c459207d
Volker Buzek <volker.buzek@camunda.com>
2025-02-14 16:42:11 +0100

    chore: mvn spotless

--- src/test/java/io/camunda/connector/sap/rfc/E2eTest.java
+++ src/test/java/io/camunda/connector/sap/rfc/E2eTest.java
@@ -3,7 +3,6 @@ package io.camunda.connector.sap.rfc;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import io.camunda.zeebe.client.ZeebeClient;
-
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
